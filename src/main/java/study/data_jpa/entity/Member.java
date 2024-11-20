c8c0fb78110714d60227bfc23c6b7bbbefc0f74d
joonhyeok.lim <dkttkemf@gmail.com>
2024-11-20 17:03:10 +0900

    쿼리메소드 테스트

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -49,6 +49,11 @@ public class Member {
         }
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMmembers().add(this);
--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package study.data_jpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.data_jpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findHelloBy();
+
 }
