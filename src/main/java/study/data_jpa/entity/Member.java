115aaa9cbad4e39f3c929178637ccc58b6e2aa19
joonhyeok.lim <dkttkemf@gmail.com>
2024-11-20 15:57:51 +0900

    기본 도메인 코딩(Member <- team)

--- src/main/java/study/data_jpa/entity/Member.java
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -1,27 +1,56 @@
 package study.data_jpa.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 // @Setter
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-        // JPA 표준스펙에 디폴트 생성자가 필수이기에
-        // JPA Proxy 기술을 쓸 때 객체를 만들어내고 하기 떄문에 상속을 허용해야함
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+    //
+    // protected Member() {
+    // // JPA 표준스펙에 디폴트 생성자가 필수이기에
+    // // JPA Proxy 기술을 쓸 때 객체를 만들어내고 하기 떄문에 상속을 허용해야함
+    // }
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            this.team = team;
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMmembers().add(this);
+    }
 }
