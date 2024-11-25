edf2bd24019b7ccec2ece3dd4c9e4b316ae6812d
joonhyeok.lim <dkttkemf@gmail.com>
2024-11-25 14:06:04 +0900

    JPA 페이징/정렬 처리

--- src/main/java/study/data_jpa/dto/MemberDto.java
+++ src/main/java/study/data_jpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.data_jpa.dto;
 
 import lombok.Data;
+import study.data_jpa.entity.Member;
 
 @Data
 public class MemberDto {
@@ -13,5 +14,9 @@ public class MemberDto {
         this.username = username;
         this.teamName = teamName;
     }
-
+    
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 }
